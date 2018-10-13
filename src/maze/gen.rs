006c96e5d8565596c863fd068ce6704b47d9d63b
caelum100 <caelum12321@gmail.com>
2018-10-13 10:22:19 -0600

    Fix compilation error

--- src/maze/gen.rs
+++ src/maze/gen.rs
@@ -9,6 +9,7 @@ use super::*;
 use rand;
 use self::rand::Rng;
 use self::rand::random;
+use std::ops::IndexMut;
 use std::collections::HashMap;
 use std::ops::Add;
 use petgraph::*;
@@ -17,7 +18,7 @@ use petgraph::graph::NodeIndex;
 struct Ctx {
     maze: Graph<Cell, u32, Undirected>,
     stack: Vec<u32>,
-    pos: u32,
+    pos: usize,
 }
 
 #[derive(Clone, Copy, Debug, Hash)]
@@ -36,7 +37,13 @@ pub fn gen_maze(width: usize, height: usize) {
 
     let mut running = true;
     loop {
-        ctx.maze.find
+        let index = NodeIndex::new(ctx.pos);
+        {
+            let cell = ctx.maze.index_mut(index);
+            cell.visited = true;
+        }
+
+
 
         if !running {
             break;
@@ -45,20 +52,41 @@ pub fn gen_maze(width: usize, height: usize) {
 }
 
 fn fill_graph(maze: &mut Graph<Cell, u32, Undirected>, width: usize, height: usize) {
-    for w in 0..width {
-        for h in 0..height {
+    for h in 0..height {
+        for w in 0..width {
             maze.add_node(Cell {
                 visited: false
             });
         }
     }
     // Connect all cells with walls
-    for w in 0..width {
-        for h in 0..height {
-            let index_num = w * height + h;
+    for h in 0..height {
+        for w in 0..width {
+            let index_num = h * width + w;
             let index = NodeIndex::new(index_num);
 
-            // TODO finish
+            // Above
+            if h > 0 {
+                maze.add_edge(index, NodeIndex::new(index_num - width), 0);
+            }
+            // Right
+            if w < width - 1 {
+                maze.add_edge(index, NodeIndex::new(index_num + 1), 0);
+            }
+            // Below
+            if h < height - 1 {
+                maze.add_edge(index, NodeIndex::new(index_num + width), 0);
+            }
+            // Left
+            if w > 0 {
+                maze.add_edge(index, NodeIndex::new(index_num - 1), 0);
+            }
         }
     }
+}
+
+fn check_edges(maze: &mut Graph<Cell, u32, Undirected>, pos: usize) -> bool {
+    // Return false if there are no unvisited adjacent cells
+    // TODO
+    true
 }
